c096841f8cf813448db821e0a6f2f42762d8942c
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-08-08 04:34:24 +0530

    implementing collision

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { lavender } from "color-name";
-
 let simpleLevelPlan = `
 ......................
 ..#................#..
@@ -20,7 +18,6 @@ class Level {
     this.height = rows.length;
     this.width = rows[0].length;
     this.startActors = [];
-
     this.rows = rows.map((row, y) => {
       return row.map((ch, x) => {
         let type = levelChars[ch];
@@ -32,7 +29,7 @@ class Level {
   }
 }
 
-//defining State to track the state of running game
+//to track the state of our running gaame we define a State class.
 
 class State {
   constructor(level, actors, status) {
@@ -44,81 +41,222 @@ class State {
   static start(level) {
     return new State(level, level.startActors, "playing");
   }
+
   get player() {
-    return this.actors.find((a) => a == "player");
+    return this.actors.find((a) => a.type == "player");
   }
 }
+
+//the Vec class is used for our two dimentional position and size of actors
 
 class Vec {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
- plus(other){
-     return new Vec(this.x+other.x,this.y+other.y)
- }
- times(factor){
-     return new Vec(this.x*factor,this.y*factor)
- }
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
+  }
+}
+//defining actors
+//player has a speed property which stores the current speed which is used to calculate momentum and gravity
 
+class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
+
+  get type() {
+    return "player";
+  }
+
+  static create(pos) {
+    return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+  }
 }
 
-
-//creating player
-class Player{
-    constructor(pos,speed){
-        this.pos = pos;
-        this.speed = speed;
-
-    }
-    get type(){
-        return 'player'
-    }
-    static create(pos){
-        return new Player(pos.plus(new Vec(0,-0.5))),
-        new Vec(0,0);
-    }
-
-}
 Player.prototype.size = new Vec(0.8, 1.5);
 
-// creating lava 
+//lava
 
-class Lava{
-    constructor(pos,speed,reset){
-        this.pos = pos;
-        this.speed = speed;
-        this.reset = reset;
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() {
+    return "lava";
+  }
+
+  static create(pos, ch) {
+    if (ch == "=") {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch == "|") {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch == "v") {
+      return new Lava(pos, new Vec(0, 3), pos);
     }
-    get type(){
-        return 'lava'
-    }
-    static create(pos,ch){
-        if(ch == "="){
-            return new Lava(pos,new Vec(2,0));
-        }else if(ch == '|'){
-            return new Lava(pos,new Vec(0,3))
-        }else if(ch == 'v'){
-            return new lavender(pos,new Vec(0,3),pos)
-        }
-    }
+  }
 }
 
-Lava.prototype.size= new Vec(1,1);
+Lava.prototype.size = new Vec(1, 1);
 
-//creating coin 
-class Coin{
-    constructor(pos,basepos,wobble){
-        this.pos = pos;
-        this.basePos = basepos;
-        this.wobble= wobble;
-    }
-    get type(){
-        return 'coin'
-    }
-    static create(pos){
-        let basePos = pos.plus(new Vec(0.2,0.1))
-        return new Coin(basePos,basePos,Math.random()*Math.PI*2)
-    }
+// to make the coin slight vertical back-forth motion we use wobble
+
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() {
+    return "coin";
+  }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+  }
 }
-Coin.prototype.size = new Vec(0.6,0.6)
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+//defining levelChars
+const levelChars = {
+  ".": "empty",
+  "#": "wall",
+  "+": "lava",
+  "@": Player,
+  'o': Coin,
+  "=": Lava,
+  "|": Lava,
+  'v': Lava,
+};
+
+// let simpleLevel = new Level(simpleLevelPlan);
+
+//drawing part
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+// a display is created by giving it a parent element to which it should append itself and a level object
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt("div", { class: "game" }, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  clear() {
+    this.dom.remove();
+  }
+}
+
+//drawGrid
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${level.width * scale}px`,
+    },
+    ...level.rows.map((row) =>
+      elt(
+        "tr",
+        { style: `height: ${scale}px` },
+        ...row.map((type) => elt("td", { class: type }))
+      )
+    )
+  );
+}
+//drawActors
+function drawActors(actors) {
+  return elt(
+    "div",
+    {},
+    ...actors.map((actor) => {
+      let rect = elt("div", { class: `actor ${actor.type}` });
+      rect.style.width = `${actor.size.x * scale}px`;
+      rect.style.height = `${actor.size.y * scale}px`;
+      rect.style.left = `${actor.pos.x * scale}px`;
+      rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    })
+  );
+}
+
+//by using syncState we can display the given state and removes the previous given state
+
+DOMDisplay.prototype.syncState = function (state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+//scrollPlayerIntoView method
+
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  // The viewport
+  let left = this.dom.scrollLeft,
+    right = left + width;
+  let top = this.dom.scrollTop,
+    bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+  if (center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.dom.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+};
+console.log("jyfduytcgt");
+
+//motion and collision
+
+Level.prototype.touches = function(pos,size,type){
+  let xStart = Math.floor(pos.x);
+  let xEnd = Math.ceil(pos.x+size.x);
+  let yStart = Math.floor(pos.y);
+  let yEnd = Math.ceil(pos.y+size.y)
+
+  for(let y=yStart;y<yEnd;y++){
+    for(let x = xStart;x<xEnd;x++){
+      let isOutside = x<0||x>=this.width||y<0||y>=this.height;
+      let here = isOutside ? 'wall':this.rows[y][x];
+      if(here == type) return type
+    }
+  }
+  return false;
+}
