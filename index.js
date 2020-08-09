65b13447870f9acfc818d4bbb99dbb42f1859577
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-08-09 15:22:23 +0530

    run animation,level,game

--- index.js
+++ index.js
@@ -300,6 +300,8 @@ Coin.prototype.collide = function (state) {
   if (!filtered.some((a) => a.type == "coin")) status = "won";
   return new State(state.level, filtered, status);
 };
+
+
 //actor updates
 
 Lava.prototype.update = function (time, state) {
@@ -368,3 +370,55 @@ function trackKeys(keys) {
 }
 
 const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+// Running the animation
+
+function runAnimation(frameFunc) {
+  let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      let timeStep = Math.min(time - lastTime, 100) / 1000;
+      if (frameFunc(timeStep) === false) return;
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+// run level
+
+function runLevel(level, Display) {
+  let display = new Display(document.body, level);
+  let state = State.start(level);
+  let ending = 1;
+  return new Promise((resolve) => {
+    runAnimation((time) => {
+      state = state.update(time, arrowKeys);
+      display.syncState(state);
+      if (state.status == "playing") {
+        return true;
+      } else if (ending > 0) {
+        ending -= time;
+        return true;
+      } else {
+        display.clear();
+        resolve(state.status);
+        return false;
+      }
+    });
+  });
+}
+
+// run game
+
+async function runGame(plans, Display) {
+  for (let level = 0; level < plans.length; ) {
+    let status = await runLevel(new Level(plans[level]), Display);
+    if (status == "won") level++;
+  }
+  console.log("You've won!");
+}
+
+runGame(levelPlan, DOMDisplay);
+
